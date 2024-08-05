a52dd2c2b252ce6ba0dd2ae199253e7df18ba53f
Marcos Riverola <marcosriverola@gmail.com>
2024-08-05 12:49:35 +0200

    Connection to openAI and working creation of placeholders

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -8,13 +8,20 @@ app.use(cors())
 app.use(express.json())
 
 app.post('/api/v1/compile', async (req, res) => {
-    const { component } = req.body
+    const { component, apiKey } = req.body
 
     if (!component) {
         return res.status(400).json({ error: 'Component is required' })
     }
 
-    const result = await CompilerService.compileCode(component)
+    let result = null
+
+    try {
+        result = await CompilerService.compileCode(component, apiKey)
+    } catch (e) {
+        console.error("Error compiling code", e)
+        return res.status(500).json({ error: 'Could not compile the code' })
+    }
 
     res.json({ html: result })
 })
--- apps/api/src/services/compiler.ts
+++ apps/api/src/services/compiler.ts
@@ -1,5 +1,68 @@
 import { build } from 'esbuild';
 import path from 'node:path'
+import { generateText } from 'ai'
+import { createOpenAI } from '@ai-sdk/openai'
+
+const extractFirstCodeBlock = (input: string) => {
+
+    console.log("INPUT:", input)
+
+    const pattern = /```(\w+)?\n([\s\S]+?)\n```/g;
+
+    let matches;
+    while ((matches = pattern.exec(input)) !== null) {
+        const language = matches[1];
+        const codeBlock = matches[2];
+        if (language === undefined || language === "jsx" || language === "javascript") {
+            console.log("CODE BLOCK:", codeBlock)
+            return codeBlock as string;
+        }
+    }
+
+    throw new Error("No code block found in input");
+};
+
+
+const getAIGeneratedComponent = async (code: string, key: string) => {
+    console.log("KEY:", key)
+
+    const openAI = createOpenAI({
+        apiKey: key
+    })
+
+    const { text } = await generateText({
+        model: openAI('gpt-4-turbo'),
+        temperature: 0,
+        maxTokens: 1000,
+        messages: [
+            {
+                role: 'system',
+                content: `
+            You are a programming assistant.
+            You are going to recieve a react component either in Typescript or Javascript.
+            Create a react component using Tailwind.
+            You are only going to reply with the component code. Don't output any extra text.
+            The component you generate will not add any dependency other than anything that is needed from React.
+            Do it using the animation from tailwind animate-pulse.
+            Don't add any useState or useEffect hooks.
+            You are just going to create a loading placeholder component that will be displayed while the data from the real component
+            is being fetched.
+            Don't add any text that says loading. Just created the loading component and substitute the elements of the real components
+            with elements using the animate-pulse animation to give a loading look and feel.
+            The code block should be a jsx code block.
+            Don't use any other color than gray-300.
+            Don't import any other components.
+        `
+            },
+            {
+                role: 'user',
+                content: code
+            }
+        ]
+    })
+
+    return text
+}
 
 // Transforms the TSX code to JS using esbuild
 const esbuildCompile = async (code: string) => {
@@ -28,13 +91,14 @@ const esbuildCompile = async (code: string) => {
                         // Intercept import of Placeholder.jsx
                         build.onResolve({ filter: /^\.\/placeholder\.jsx$/ }, args => ({
                             path: args.path,
-                            namespace: 'virtual'
+                            namespace: 'virtual',
                         }));
 
                         // Load virtual content for Placeholder.jsx
                         build.onLoad({ filter: /^\.\/placeholder\.jsx$/, namespace: 'virtual' }, args => ({
                             contents: code,
-                            loader: 'jsx'
+                            loader: 'jsx',
+                            resolveDir: path.join(process.cwd(), 'compiled-app')
                         }));
                     }
                 }
@@ -55,10 +119,26 @@ const esbuildCompile = async (code: string) => {
 };
 
 export const CompilerService = {
-    compileCode: async (code: string) => {
+    compileCode: async (code: string, key: string) => {
+
+        let generatedCode = null
+
+        try {
+            generatedCode = await getAIGeneratedComponent(code, key)
+            generatedCode = extractFirstCodeBlock(generatedCode);
+        } catch (e) {
+            console.error("Error generating the code with the AI", e)
+            throw e
+        }
 
         // Transform the code from TSX to JS
-        const output = await esbuildCompile(code);
+        let output = null
+        try {
+            output = await esbuildCompile(generatedCode)
+        } catch (e) {
+            console.error("Error compiling the code", e)
+            throw e
+        }
 
         const html = `<!DOCTYPE html>
             <html lang="en">
